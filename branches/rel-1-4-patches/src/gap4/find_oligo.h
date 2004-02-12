d298c8e2688bf64131b9eca1257f97664d618b11
(no author) <(no author)@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2004-02-12 12:24:10 +0000

    This commit was manufactured by cvs2svn to create branch 'rel-1-4-patches'.
    git-svn-id: svn://svn.code.sf.net/p/staden/code/staden@230 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

---
+++ branches/rel-1-4-patches/src/gap4/find_oligo.h
@@ -0,0 +1,9 @@
+#include "io_utils.h"
+
+int find_oligos(GapIO *io,
+		int num_contigs,
+		contig_list_t *contig_array,
+		float mis_match,
+		char *string,
+		int consensus_only,
+		int in_cutoff);
