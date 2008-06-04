a4463953214a78aace0ecc1db104ef0da4c7dc5d
(no author) <(no author)@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2008-06-04 14:15:01 +0000

    This commit was manufactured by cvs2svn to create tag 'iolib-1-11-2'.
    git-svn-id: svn://svn.code.sf.net/p/staden/code/staden@1241 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

---
+++ tags/iolib-1-11-2/src/io_lib/io_lib/srf.h
@@ -0,0 +1,157 @@
+#ifndef _SRF_H_
+#define _SRF_H_
+
+#include "io_lib/ztr.h"
+#include "io_lib/mFILE.h"
+
+#define SRF_MAGIC		"SSRF"
+#define SRF_VERSION             "1.3"
+
+#define SRFB_CONTAINER 		'S'
+#define SRFB_XML		'X'
+#define SRFB_TRACE_HEADER	'H'
+#define SRFB_TRACE_BODY		'R'
+#define SRFB_INDEX		'I'
+
+/*--- Public structures */
+
+/* Container header - several per file */
+typedef struct {
+    char block_type;
+    char version[256];
+    char container_type;
+    char base_caller[256];
+    char base_caller_version[256];
+} srf_cont_hdr_t;
+
+/* Trace header - several per container */
+typedef struct {
+    char block_type; 
+    char read_prefix_type;
+    char id_prefix[256];
+    uint32_t trace_hdr_size;
+    unsigned char *trace_hdr;
+} srf_trace_hdr_t;
+
+/* Trace body - several per trace header */
+typedef struct {
+    char block_type;
+    int read_id_length;
+    char read_id[256];
+    unsigned char flags;
+    uint32_t trace_size;
+    unsigned char *trace;
+} srf_trace_body_t;
+
+/* XML - NCBI TraceInfo data block */
+typedef struct {
+    uint32_t xml_len;
+    char *xml;
+} srf_xml_t;
+
+#define SRF_READ_FLAG_BAD_MASK       (1<<0)
+#define SRF_READ_FLAG_WITHDRAWN_MASK (1<<1)
+#define SRF_READ_FLAG_USER_MASK      (7<<5)
+
+/* Index - one per file */
+typedef struct {
+    uint32_t next_block_offset;
+    char block_type;
+} srf_index_t;
+
+/* Indexing */
+typedef struct {
+    char     magic[4];
+    char     version[4];
+    uint64_t size;
+    uint32_t n_container;
+    uint32_t n_data_block_hdr;
+    uint64_t n_buckets;
+    int8_t   index_type;
+    int8_t   dbh_pos_stored_sep;
+    char     dbh_file[256];
+    char     cont_file[256];
+    int      index_hdr_sz; /* size of the above data on disk */
+} srf_index_hdr_t;
+
+/* Master SRF object */
+typedef struct {
+    FILE *fp;
+
+    /* Cached copies of each of the most recent chunk types loaded */
+    srf_cont_hdr_t    ch;
+    srf_trace_hdr_t   th;
+    srf_trace_body_t  tb;
+    srf_xml_t         xml;
+    srf_index_hdr_t   hdr;
+
+    /* Private: cached data for use by srf_next_ztr */
+    ztr_t *ztr;
+    mFILE *mf;
+    long mf_pos, mf_end;
+} srf_t;
+
+#define SRF_INDEX_MAGIC    "Ihsh"
+#define SRF_INDEX_VERSION  "1.01"
+
+
+/*--- Initialisation */
+srf_t *srf_create(FILE *fp);
+srf_t *srf_open(char *fn, char *mode);
+void srf_destroy(srf_t *srf, int auto_close);
+
+/*--- Base type I/O methods */
+
+int srf_write_pstring(srf_t *srf, char *str);
+int srf_write_pstringb(srf_t *srf, char *str, int length);
+int srf_read_pstring(srf_t *srf, char *str);
+
+int srf_read_uint32(srf_t *srf, uint32_t *val);
+int srf_write_uint32(srf_t *srf, uint32_t val);
+
+int srf_read_uint64(srf_t *srf, uint64_t *val);
+int srf_write_uint64(srf_t *srf, uint64_t val);
+
+/*--- Mid level I/O - srf block */
+srf_cont_hdr_t *srf_construct_cont_hdr(srf_cont_hdr_t *ch,
+				       char *bc,
+				       char *bc_version);
+void srf_destroy_cont_hdr(srf_cont_hdr_t *ch);
+int srf_read_cont_hdr(srf_t *srf, srf_cont_hdr_t *ch);
+int srf_write_cont_hdr(srf_t *srf, srf_cont_hdr_t *ch);
+
+int srf_read_xml(srf_t *srf, srf_xml_t *xml);
+int srf_write_xml(srf_t *srf, srf_xml_t *xml);
+
+srf_trace_hdr_t *srf_construct_trace_hdr(srf_trace_hdr_t *th,
+					 char *prefix,
+					 unsigned char *header,
+					 uint32_t header_sz);
+void srf_destroy_trace_hdr(srf_trace_hdr_t *th);
+int srf_read_trace_hdr(srf_t *srf, srf_trace_hdr_t *th);
+int srf_write_trace_hdr(srf_t *srf, srf_trace_hdr_t *th);
+
+srf_trace_body_t *srf_construct_trace_body(srf_trace_body_t *th,
+					   char *suffix,
+					   int suffix_len,
+					   unsigned char *body,
+					   uint32_t body_size,
+					   unsigned char flags);
+void srf_destroy_trace_body(srf_trace_body_t *th);
+int srf_write_trace_body(srf_t *srf, srf_trace_body_t *th);
+int srf_read_trace_body(srf_t *srf, srf_trace_body_t *th, int no_trace);
+
+int srf_read_index_hdr(srf_t *srf, srf_index_hdr_t *hdr, int no_seek);
+int srf_write_index_hdr(srf_t *srf, srf_index_hdr_t *hdr);
+
+/*--- Higher level I/O functions */
+mFILE *srf_next_trace(srf_t *srf, char *name);
+ztr_t *srf_next_ztr(srf_t *srf, char *name, int filter_mask);
+
+int srf_next_block_type(srf_t *srf); /* peek ahead */
+int srf_next_block_details(srf_t *srf, uint64_t *pos, char *name);
+
+int srf_find_trace(srf_t *srf, char *trace,
+		   uint64_t *cpos, uint64_t *hpos, uint64_t *dpos);
+
+#endif /* _SRF_H_ */
---
+++ tags/iolib-1-11-2/src/io_lib/progs/srf2fastq.c
@@ -0,0 +1,160 @@
+/*
+ * ======================================================================
+ * This software has been created by Genome Research Limited (GRL).
+ *
+ * GRL hereby grants permission to use, copy, modify and distribute
+ * this software and its documentation for non-commercial purposes
+ * without fee at the user's own risk on the basis set out below.
+ *
+ * GRL neither undertakes nor accepts any duty whether contractual or
+ * otherwise in connection with the software, its use or the use of
+ * any derivative, and makes no representations or warranties, express
+ * or implied, concerning the software, its suitability, fitness for
+ * a particular purpose or non-infringement.
+ *
+ * In no event shall the authors of the software or GRL be responsible
+ * or liable for any loss or damage whatsoever arising in any way
+ * directly or indirectly out of the use of this software or its
+ * derivatives, even if advised of the possibility of such damage.
+ *
+ * Our software can be freely distributed under the conditions set out
+ * above, and must contain this copyright notice.
+ * ======================================================================
+ */
+
+/*
+ * This performs a linear (non-indexed) search for a trace in an SRF archive.
+ *
+ * It's not intended as a suitable production program or as a library of code
+ * to use, but as a test and benchmark statistic.
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <io_lib/Read.h>
+#include <io_lib/misc.h>
+#include <io_lib/ztr.h>
+#include <io_lib/srf.h>
+
+static char qlookup[256];
+void init_qlookup(void) {
+    int i;
+    for (i = -128; i < 128; i++) {
+        qlookup[i+128] = '!' + (int)((10*log(1+pow(10, i/10.0))/log(10)+.499));
+    }
+}
+
+/* ------------------------------------------------------------------------ */
+
+#define MAX_READ_LEN 1024
+void ztr2fastq(ztr_t *z, char *name, int calibrated) {
+    int i, nc, seq_len;
+    char buf[MAX_READ_LEN*2 + 512 + 6];
+    char *seq, *qual, *sdata, *qdata;
+    ztr_chunk_t **chunks;
+
+    /* Extract the sequence only */
+    chunks = ztr_find_chunks(z, ZTR_TYPE_BASE, &nc);
+    if (nc != 1) {
+	fprintf(stderr, "Zero or greater than one BASE chunks found.\n");
+	return;
+    }
+    uncompress_chunk(z, chunks[0]);
+    sdata = chunks[0]->data+1;
+    seq_len = chunks[0]->dlength-1;
+
+    /* Extract the quality */
+    if (calibrated)
+	chunks = ztr_find_chunks(z, ZTR_TYPE_CNF1, &nc);
+    else
+	chunks = ztr_find_chunks(z, ZTR_TYPE_CNF4, &nc);
+
+    if (nc != 1) {
+	fprintf(stderr, "Zero or greater than one CNF chunks found.\n");
+	return;
+    }
+    uncompress_chunk(z, chunks[0]);
+    qdata = chunks[0]->data+1;
+
+    /* Construct fastq entry */
+    seq = buf;
+    *seq++ = '@';
+    while (*name)
+	*seq++ = *name++;
+    *seq++ = '\n';
+
+    qual = seq + seq_len;
+    *qual++ = '\n';
+    *qual++ = '+';
+    *qual++ = '\n';
+
+    for (i = 0; i < seq_len; i++) {
+	if (*sdata != '.') {
+	    *seq++ = *sdata++;
+	} else {
+	    *seq++ = 'N';
+	    sdata++;
+	}
+	*qual++ = calibrated
+	    ? *qdata++ + '!'
+	    : qlookup[*qdata++ + 128];
+    }
+    *qual++ = '\n';
+
+    fwrite(buf, 1, qual - buf, stdout);
+}
+
+/* ------------------------------------------------------------------------ */
+void usage(void) {
+    fprintf(stderr, "Usage: srf2fastq [-c] [-C] archive_name ...\n");
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    int calibrated = 0;
+    int mask = 0, i;
+
+    /* Parse args */
+    for (i = 1; i < argc && argv[i][0] == '-'; i++) {
+	if (!strcmp(argv[i], "-")) {
+	    break;
+	} else if (!strcmp(argv[i], "-C")) {
+	    mask = SRF_READ_FLAG_BAD_MASK;
+	} else if (!strcmp(argv[i], "-c")) {
+	    calibrated = 1;
+	} else {
+	    usage();
+	}
+    }    
+
+    if (i == argc) {
+	usage();
+    }
+
+    read_sections(READ_BASES);
+    init_qlookup();
+
+    for (; i < argc; i++) {
+	char *ar_name;
+	srf_t *srf;
+	char name[512];
+	ztr_t *ztr;
+
+	ar_name = argv[i];
+
+	if (NULL == (srf = srf_open(ar_name, "r"))) {
+	    perror(ar_name);
+	    return 4;
+	}
+
+
+	while (NULL != (ztr = srf_next_ztr(srf, name, mask))) {
+	    ztr2fastq(ztr, name, calibrated);
+	    delete_ztr(ztr);
+	}
+
+	srf_destroy(srf, 1);
+    }
+
+    return 0;
+}
